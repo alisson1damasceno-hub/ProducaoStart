fb45a19eff5bfc5475a35dbf7a27e976ac113d82
Alisson <alisson1damasceno@gmail.com>
2026-07-09 10:56:21 -0300

    fix: remove console.log de debug do middleware e restaura .env.local.example

--- middleware.ts
+++ middleware.ts
@@ -13,8 +13,7 @@ export async function middleware(request: NextRequest) {
   }
 
   const response = NextResponse.next();
-  console.log("URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log("KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+  
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
